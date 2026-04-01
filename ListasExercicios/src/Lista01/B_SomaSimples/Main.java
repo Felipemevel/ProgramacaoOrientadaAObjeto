6594f20e200a0573c430c1e1ee564540eb9c0b73
Felipe Nascimento <lipe.conta1632@gmail.com>
2026-04-01 09:45:36 -0300

    Segunda questão resolvida

---
+++ ListasExercicios/src/Lista01/B_SomaSimples/Main.java
@@ -0,0 +1,25 @@
+package Lista01.B_SomaSimples;
+
+import java.io.IOException;
+import java.util.Scanner;
+
+public class Main {
+
+    public static int soma(int a, int b){
+        int x = a + b;
+        return x;
+    }
+
+    public static void main(String[] args) throws IOException {
+
+        Scanner scanner = new Scanner(System.in);
+
+        int a = scanner.nextInt();
+        scanner.nextLine();
+        int b = scanner.nextInt();
+
+        System.out.println("SOMA = " + soma(a, b));
+
+    }
+
+}

f60852b61c8573f5c0507d3971283dad801d660f
Felipe Mevel <109548375+Felipemevel@users.noreply.github.com>
2026-04-01 15:54:37 -0300

    Oitava questão resolvida

---
+++ ListasExercicios/src/Lista01/H-Consumo/Main.java
@@ -0,0 +1,18 @@
+import java.io.IOException;
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+
+        Scanner scanner = new Scanner(System.in);
+
+        int distancia = scanner.nextInt();
+        double combustivel = scanner.nextDouble();
+
+        double consumo = distancia/combustivel;
+
+        System.out.printf("%.3f km/l\n", consumo);
+    }
+
+}

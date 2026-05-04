1a28a8c7010b2410f5ca5e3acc39c70ffc138079
Felipe Mevel <109548375+Felipemevel@users.noreply.github.com>
2026-05-04 17:13:49 -0300

    Disciplina corrigdo

--- ListasExercicios/src/ListasPOO/Lista01/B_Disciplina/Disciplina.java
+++ ListasExercicios/src/ListasPOO/Lista01/B_Disciplina/Disciplina.java
@@ -1,7 +1,5 @@
 package Lista01.B_Disciplina;
 
-import java.util.Scanner;
-
 public class Disciplina {
 
     String nomeDisciplina;
@@ -16,24 +14,22 @@ public class Disciplina {
     }
 
     public double mediaParcial(double notaBim1, double notaBim2){
-        return ((notaBim1*2)+(notaBim2+3))/2+3;
+        return ((notaBim1*2)+(notaBim2*3))/(2+3);
     }
 
     public double mediaFinal(double notaBim1, double notaBim2, double notaFinal){
         return (mediaParcial(notaBim1, notaBim2)+(notaFinal))/2;
     }
 
-    public boolean testeAprovacao(){
-        double mediaParcial = mediaParcial(notaBim1, notaBim2);
-        double mediaFinal = mediaFinal(notaBim1, notaBim2, notaFinal);
+    public boolean testeAprovacao() {
+        double mp = mediaParcial(notaBim1, notaBim2);
 
-        if(mediaParcial < 60){
-            return true;
-        } else if (mediaFinal < 60){
-            return false;
-        } else {
-            return false;
+        if (this.notaFinal == 0) {
+            return mp >= 60;
         }
 
+        return mediaFinal(notaBim1, notaBim2, notaFinal) >= 60;
     }
+
+
 }

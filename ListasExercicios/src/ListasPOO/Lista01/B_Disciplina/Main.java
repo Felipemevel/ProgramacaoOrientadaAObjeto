55aac28ef31257a27d00522d2fb45a29cdcb6861
Felipe Mevel <109548375+Felipemevel@users.noreply.github.com>
2026-05-04 17:13:13 -0300

    Main corrigido

--- ListasExercicios/src/ListasPOO/Lista01/B_Disciplina/Main.java
+++ ListasExercicios/src/ListasPOO/Lista01/B_Disciplina/Main.java
@@ -5,15 +5,12 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        System.out.printf("Digite o nome da disciplina: \n");
+        System.out.printf("Digite o nome da disciplina: ");
         String nome = scanner.nextLine();
-        scanner.nextLine();
-        System.out.printf("Digite a nota do primeiro bimestre: \n");
+        System.out.printf("\nDigite a nota do primeiro bimestre: ");
         double nota1 = scanner.nextDouble();
-        scanner.nextLine();
-        System.out.printf("Digite a nota do segundo bimestre: \n");
+        System.out.printf("\nDigite a nota do segundo bimestre: ");
         double nota2 = scanner.nextDouble();
-        scanner.nextLine();
 
         Disciplina disciplina = new Disciplina(nome, nota1, nota2);
 
@@ -24,16 +21,16 @@ public class Main {
             disciplina.notaFinal = notaFinal;
             if (!disciplina.testeAprovacao()){
                 System.out.println("==== REPROVADO ====");
-                System.out.println("= Nota: " + disciplina.mediaFinal(nota1, nota2, notaFinal));
+                System.out.println("= Disciplina: " + nome + " | Nota: " + disciplina.mediaFinal(nota1, nota2, notaFinal));
                 System.out.println("===================");
             } else {
                 System.out.println("==== APROVADO ====");
-                System.out.println("= Nota: " + disciplina.mediaFinal(nota1, nota2, notaFinal));
+                System.out.println("= Disciplina: " + nome + " | Nota: " + disciplina.mediaFinal(nota1, nota2, notaFinal));
                 System.out.println("==================");
             }
         } else {
             System.out.println("==== APROVADO ====");
-            System.out.println("= Nota: " + disciplina.mediaParcial(nota1, nota2));
+            System.out.println("= Disciplina: "+ nome + " | Nota: " + disciplina.mediaParcial(nota1, nota2));
             System.out.println("==================");
         }
     }

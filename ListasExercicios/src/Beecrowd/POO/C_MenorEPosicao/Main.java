d5ab36d6250441733b413c426f70b5588ee78046
Felipe Mevel <109548375+Felipemevel@users.noreply.github.com>
2026-05-12 14:26:02 -0300

    Questão concluida

---
+++ ListasExercicios/src/Beecrowd/POO/C_MenorEPosicao/Main.java
@@ -0,0 +1,58 @@
+package POO.C_MenorEPosicao;
+
+import java.util.Scanner;
+
+class Vetor {
+    private int[] lista;
+    private int menor;
+    private int posicaoDoMenor;
+
+    public Vetor(){
+
+    }
+    public Vetor(int[] lista){
+        this.lista = lista;
+    }
+
+    public void encontrarValorEPosicao(){
+        int recordeMenor = 0;
+        int idxMenor = 0;
+
+        for (int i = 0; i < lista.length; i++){
+            if (lista[i] < recordeMenor){
+                recordeMenor = lista[i];
+                idxMenor = i;
+
+            }
+        }
+
+        System.out.println("Menor valor: " + recordeMenor + "\nPosicao: " + idxMenor);
+    }
+
+}
+
+
+
+public class Main {
+
+    public static int[] montarLista(int qtdElementosDaLista, Scanner sc){
+        int[] listaMontada = new int[qtdElementosDaLista];
+
+        for (int i = 0; i < qtdElementosDaLista; i++){
+            listaMontada[i] = sc.nextInt();
+        }
+
+        return listaMontada;
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int qtdElementos = sc.nextInt();
+        int[] lista = montarLista(qtdElementos, sc);
+
+        Vetor novoVetor = new Vetor(lista);
+
+        novoVetor.encontrarValorEPosicao();
+    }
+
+}

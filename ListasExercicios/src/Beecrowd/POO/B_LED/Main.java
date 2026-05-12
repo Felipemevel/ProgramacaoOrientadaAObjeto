cbc36ce57207999310b8cf28d6a430093cb4b175
Felipe Mevel <109548375+Felipemevel@users.noreply.github.com>
2026-05-12 13:55:17 -0300

    Questão concluída

--- ListasExercicios/src/Beecrowd/POO/B_LED/Main.java
+++ ListasExercicios/src/Beecrowd/POO/B_LED/Main.java
@@ -1,18 +1,17 @@
 package POO.B_LED;
 
 import java.util.ArrayList;
-import java.util.List;
 import java.util.Scanner;
 
 class Led {
-    private int id;
+    private char id;
     private int leds;
 
     public Led(){
 
     }
 
-    public Led(int id, int leds){
+    public Led(char id, int leds){
         this.id = id;
         this.leds = leds;
     }
@@ -24,16 +23,6 @@ class Led {
 
 public class Main {
 
-    public static List<Integer> dividirInput(int painel, List<Integer> listaPainel){
-        if (painel < 10){
-            listaPainel.add(painel);
-        } else {
-            dividirInput((painel / 10), listaPainel);
-            listaPainel.add(painel % 10);
-        }
-        return listaPainel;
-    }
-
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
@@ -41,29 +30,24 @@ public class Main {
         ArrayList<Led> ledsNums = new ArrayList<>();
 
         int qtdOperacoes = sc.nextInt();
+        sc.nextLine();
         int[] qtdLeds = {6, 2, 5, 5, 4, 5, 6, 3, 7, 6};
 
-
-
         for (int i = 0; i < qtdLeds.length; i++){
-            ledsNums.add(new Led(i, qtdLeds[i]));
+            char idTemp = (char) i;
+            ledsNums.add(new Led(idTemp, qtdLeds[i]));
         }
-        while (qtdOperacoes != 0){
-            int painel = sc.nextInt();
-            listaPainel.clear();
+        while(qtdOperacoes != 0){
+            String painel = sc.nextLine();
+            int ledsTotais = 0;
 
-            dividirInput(painel, listaPainel);
-            int totalLeds = 0;
-
-
-
-            for (int i = 0; i < listaPainel.size(); i++){
-                int digito = listaPainel.get(i);
-                totalLeds += ledsNums.get(digito).getLeds();
+            for (int i = 0; i < painel.length(); i++){
+                char algarismo = painel.charAt(i);
+                int indice = algarismo - '0';
+                ledsTotais += ledsNums.get(indice).getLeds();
             }
-            System.out.println(totalLeds + " leds");
+            System.out.println(ledsTotais + " leds");
             qtdOperacoes--;
-
         }
     }
 }
